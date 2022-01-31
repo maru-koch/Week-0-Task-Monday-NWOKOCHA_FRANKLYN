8e0aca63f3829222c051aa442db5f0c13231fbb6
maru_koch <nwokochafranklyn@gmail.com>
2022-01-31 15:56:57 +0100

    Commit: Added solution to question - 1

---
+++ question-1.py
@@ -0,0 +1,25 @@
+
+def factorialCalcualtor(number):
+    """
+        ASSIGNMENT - Question 1
+
+        - This methods calculates the factorial of an integer using\
+          the formula n! = n(n-1)!
+
+    """
+    factorial = number
+    
+    for index in range(1, number):
+        
+        factorial *= number - index
+
+    # - you may comment out the PRINT STATEMENT
+
+    print(f"The factorial of {number} is {factorial}")
+
+    return f"The factorial for {number} is {factorial}"
+    
+
+number = int(input("Enter an Integer: "))
+
+factorialCalcualtor(number)

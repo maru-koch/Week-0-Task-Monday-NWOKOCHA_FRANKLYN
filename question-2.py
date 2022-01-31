f787c40a7bdcaa72fa35c2789af1b26b887f3c3c
maru_koch <nwokochafranklyn@gmail.com>
2022-01-31 17:01:37 +0100

    Commit: Solution to question 2 added

---
+++ question-2.py
@@ -0,0 +1,31 @@
+
+def leapYear(year):
+    """
+        - this methods determines if a year is a leap year or not.
+        - flow logic is based on the following premises:
+            - a year is a leap year if evenly divided by four
+            - excerp for century ending with 00
+            - a century year is a leap year if perfectly divided by 400
+    """
+    if len(str(year)) != 4:
+        print("Invalid Year")
+
+    if year % 4 == 0:
+
+        if str(year).endswith("00"):
+
+            if year % 400 == 0:
+
+                print("True")
+
+            else:
+
+                print("False")
+        else:
+            print("True")
+    else:
+        print("False")
+
+
+year = int(input("Enter year: "))
+leapYear(year)
